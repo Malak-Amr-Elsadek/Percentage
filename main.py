4f6f4d4ad341c53fd294913d3c7b8b48d29eee7f
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-10-31 15:47:38 +0300

    Percentage

---
+++ main.py
@@ -0,0 +1,14 @@
+Math=100
+English=100
+Science=100
+Coding=100
+Arabic=100
+
+sum=Math+English+Science+Coding+Arabic
+print("Added value is",sum)
+
+average=sum/5
+print("Average is",average)
+
+percentage=(sum/500)*100
+print("Pecentage is",percentage)
